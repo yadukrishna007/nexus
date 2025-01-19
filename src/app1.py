d314bfc0f5733bcf7d5077fe18697a2c92bca6f7
YADU KRISHNA <yadukrishnasajeev@gmail.com>
2025-01-19 12:09:31 +0530

    heatmap backend

--- src/app1.py
+++ src/app1.py
@@ -1,17 +1,26 @@
+import pymysql
 from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
+# Establish databaseconnection
+con = pymysql.connect(
+    host='localhost',
+    user='root',
+    password='root',
+    port=3307,
+    db='smartcitydb',
+    charset='utf8'
+)
+
 # Route to render the map page
 @app.route('/')
 def map_page():
-    # Dummy data points for heatmap (latitude, longitude, and weight)
-    heatmap_data = [
-        {"lon": 75.231870, "lat": 12.240140, "weight": 0.8},  # College of Engineering Trikaripur
-        {"lon": 75.233870, "lat": 12.242140, "weight": 0.6},  # Nearby point 1
-        {"lon": 75.229870, "lat": 12.238140, "weight": 0.9},  # Nearby point 2
-        {"lon": 75.235870, "lat": 12.241140, "weight": 0.7},  # Nearby point 3
-    ]
+    # Fetch heatmap data from the database as a dictionary
+    cmd = con.cursor(pymysql.cursors.DictCursor)
+    cmd.execute("SELECT temp, hum, gas, noise, lat, long FROM readings")
+    heatmap_data = cmd.fetchall()
+    con.close()
     return render_template('map.html', heatmap_data=heatmap_data)
 
 if __name__ == '__main__':
